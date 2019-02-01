2d3e773443877008898573a91d7343f8c5e094ad
okwero2014 <oscar_okwero@yahoo.com>
2019-02-01 10:18:11 +0000

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+package topTrumps;
+
+import java.util.ArrayList;
+
+public class Main {
+public static void main(String[] args) {
+		
+	   	StartGame sG= new StartGame();
+		
+		
+		
+		
+	}
+
+
+}
